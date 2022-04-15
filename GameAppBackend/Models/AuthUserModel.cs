41d5b413e12abfdad2456a5b5776b42f253d6b94
C0ppens <45225697+C0ppens@users.noreply.github.com>
2022-04-15 22:22:28 +1000

    Add base AuthUserModel

---
+++ GameAppBackend/Models/AuthUserModel.cs
@@ -0,0 +1,19 @@
+﻿namespace GameAppBackend.Models
+{
+    public class AuthUserModel
+    {
+        /// Example model for now - using these nice get set var initializers
+        public string UserName { get; set; }
+        public string Password { get; set; } 
+        public string Email { get; set; }   
+
+
+        // Constructor doin constructor tings
+        public AuthUserModel(string userName, string password, string email)
+        {
+            UserName = userName;
+            Password = password;
+            Email = email;
+        }
+    }
+}
